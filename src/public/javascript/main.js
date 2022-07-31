2105576077543988e2051e1219cf29bfa9d6931f
Dung <chungle8216@gmail.com>
2022-07-31 16:22:24 +0700

    Homepage 2

--- src/public/javascript/main.js
+++ src/public/javascript/main.js
@@ -1,4 +1,21 @@
 
+$('.cs-5').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+})
+
 $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 10,
@@ -15,4 +32,6 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+
 
