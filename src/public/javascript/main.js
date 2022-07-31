d78b62bc1d38528005dac3cf0d56b9b092c47156
Dung <chungle8216@gmail.com>
2022-07-31 20:39:24 +0700

    Homepage 3

--- src/public/javascript/main.js
+++ src/public/javascript/main.js
@@ -3,6 +3,9 @@ $('.cs-5').owlCarousel({
     loop: true,
     margin: 10,
     nav: true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
     responsive: {
         0: {
             items: 1
@@ -11,7 +14,7 @@ $('.cs-5').owlCarousel({
             items: 3
         },
         1000: {
-            items: 5
+            items: 4
         }
     }
 })
@@ -20,6 +23,9 @@ $('.owl-carousel').owlCarousel({
     loop: true,
     margin: 10,
     nav: true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
     responsive: {
         0: {
             items: 1
