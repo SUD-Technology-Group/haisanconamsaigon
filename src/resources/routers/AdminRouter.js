602be6ce3b4bb6c7337093993ee25035fadc6125
Tran Khai Hoang <hoangbo74@gmail.com>
2022-08-28 20:02:31 +0700

    fix path render views in file AdminRouter

--- src/resources/routers/AdminRouter.js
+++ src/resources/routers/AdminRouter.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const router = express.Router()
 router.get('/', (req, res) => {
-    res.render('Admin/adminHome', { layout: 'admin' })
+    res.render('Pages/Admin/adminHome', { layout: 'admin' })
 })
 
 router.get('/add-product', (req, res) => {
     const success = req.flash('success') || '';
     const error = req.flash('error') || '';
-    res.render('Admin/addProduct', { layout: 'admin', success, error })
+    res.render('Pages/Admin/addProduct', { layout: 'admin', success, error })
 })
 
 router.get('/list-product', (req, res) => {
     const success = req.flash('success') || '';
     const error = req.flash('error') || '';
-    res.render('Admin/listProduct', { layout: 'admin', success, error })
+    res.render('Pages/Admin/listProduct', { layout: 'admin', success, error })
 })
 module.exports = router
